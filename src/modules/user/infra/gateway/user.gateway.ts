255a1d48e4a055ec51f363a6a2c79906b231b432
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-28 12:36:34 -0300

    refactor: improve methods in interface using either pattern

--- src/modules/user/infra/gateway/user.gateway.ts
+++ src/modules/user/infra/gateway/user.gateway.ts
@@ -1,8 +1,23 @@
+// Entity
 import { User } from "../../domain/entity/User";
 
+// Error
+import { DatabaseError } from "../../../../shared/errors/DatabaseError.error";
+import { EntityNotFoundError } from "../../../../shared/errors/EntityNotFound.error";
+
+// Type
+import { Either } from "../../../../shared/types/Either.types";
+
 export interface UserGateway {
-  create(user: User): Promise<void>;
-  findByd(id: string): Promise<User | null>;
-  findByEmail(email: string): Promise<User | null>;
-  rename(id: string, newUsername: string): Promise<void>;
+  create(user: User): Promise<Either<DatabaseError, void>>;
+  findByd(
+    id: string
+  ): Promise<Either<DatabaseError | EntityNotFoundError, User | null>>;
+  findByEmail(
+    email: string
+  ): Promise<Either<DatabaseError | EntityNotFoundError, User | null>>;
+  rename(
+    id: string,
+    newUsername: string
+  ): Promise<Either<DatabaseError | EntityNotFoundError, void>>;
 }
