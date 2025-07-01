544138b358cb198dadcaab0c73b064205dad7120
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-07-01 18:46:41 -0300

    refactor: include either pattern in interface of usecase

--- src/shared/interfaces/usecase.interface.ts
+++ src/shared/interfaces/usecase.interface.ts
@@ -1,3 +1,9 @@
+// Type
+import type { Either } from "../types/Either.types";
+
+// Error
+import { ApplicationError } from "../errors/Application.error";
+
 export interface UseCase<InputDTO, OutputDTO> {
-  execute(input: InputDTO): Promise<OutputDTO>;
+  execute(input: InputDTO): Promise<Either<ApplicationError, OutputDTO>>;
 }
