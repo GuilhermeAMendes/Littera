69398c6e17c14a10ba639ad2e060c638998de5ff
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-07-02 01:58:35 -0300

    feat: create user factory to construct all routes in module

---
+++ src/modules/user/infra/factory/user.factory.ts
@@ -0,0 +1,23 @@
+// Repository
+import { UserRepositoryPrisma } from "../repository/prisma/User.repository";
+
+// Factory
+import {
+  bcrypt,
+  prisma,
+  UuidGenerator,
+} from "../../../../shared/factory/factory.shared";
+
+// Create
+import { CreateUserUseCase } from "../../application/use-cases/create/CreateUser.usecase";
+import { CreateUserController } from "../controllers/express/create/CreateUser.controller";
+
+export function createUserControllers() {
+  const userRepository = UserRepositoryPrisma.with(prisma);
+
+  const create = CreateUserController.create(
+    CreateUserUseCase.create(userRepository, UuidGenerator, bcrypt)
+  );
+
+  return [create];
+}
